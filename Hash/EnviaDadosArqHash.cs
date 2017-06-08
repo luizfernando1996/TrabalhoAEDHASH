9aeca9adcc574ee2cf3dfe9a514e1db1aef62fab
Luiz <luizfrnnd412@hotmail.com>
2017-06-08 00:17:18 -0300

    Push

---
+++ Hash/EnviaDadosArqHash.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class EnviaDadosArqHash: Leitura
+{
+	public EnviaDadosArqHash()
+	{
+
+	}
+}
---
+++ Hash/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Node
+{
+    private string nomeEstado;
+    private string capitalEstado;
+    private string regiaoDoEstado;
+    private int quantMunicipios;
+    private 
+    public Node(string nomeDoEstado,string capitalDoEstado,string regiaoEstado,int quantMunicipios)
+    {
+        this.nomeEstado = nomeDoEstado;
+        this.capitalEstado = capitalDoEstado;
+        this.regiaoDoEstado = regiaoEstado;
+        this.quantMunicipios = quantMunicipios;
+
+
+    }
+
+}
