c2c7df8cb5aafb227101786cb4b57d662048390f
Luiz <luizfrnnd412@hotmail.com>
2017-06-08 00:19:00 -0300

    node lista

--- Hash/Node.cs
+++ Hash/Node.cs
@@ -6,15 +6,14 @@ public class Node
     private string capitalEstado;
     private string regiaoDoEstado;
     private int quantMunicipios;
-    private 
+    private Node next;
+
     public Node(string nomeDoEstado,string capitalDoEstado,string regiaoEstado,int quantMunicipios)
     {
         this.nomeEstado = nomeDoEstado;
         this.capitalEstado = capitalDoEstado;
         this.regiaoDoEstado = regiaoEstado;
         this.quantMunicipios = quantMunicipios;
-
-
     }
 
 }
