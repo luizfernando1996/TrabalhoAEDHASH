cf9d1581d8536ab1800c37a6326350bb3a61cdf3
Luiz <luizfrnnd412@hotmail.com>
2017-06-08 00:24:26 -0300

    Getters and setters

--- Hash/Node.cs
+++ Hash/Node.cs
@@ -2,18 +2,28 @@
 
 public class Node
 {
+    #region 'Atributos'
     private string nomeEstado;
     private string capitalEstado;
     private string regiaoDoEstado;
     private int quantMunicipios;
     private Node next;
+    #endregion
 
-    public Node(string nomeDoEstado,string capitalDoEstado,string regiaoEstado,int quantMunicipios)
+    #region 'Propriedades'
+    public string NomeEstado { get => nomeEstado; set => nomeEstado = value; }
+    public string CapitalEstado { get => capitalEstado; set => capitalEstado = value; }
+    public string RegiaoDoEstado { get => regiaoDoEstado; set => regiaoDoEstado = value; }
+    public int QuantMunicipios { get => quantMunicipios; set => quantMunicipios = value; }
+    public Node Next { get => next; set => next = value; }
+    #endregion
+
+    public Node(string nomeDoEstado, string capitalDoEstado, string regiaoEstado, int quantMunicipios, Node proximo)
     {
-        this.nomeEstado = nomeDoEstado;
-        this.capitalEstado = capitalDoEstado;
-        this.regiaoDoEstado = regiaoEstado;
-        this.quantMunicipios = quantMunicipios;
+        this.NomeEstado = nomeDoEstado;
+        this.CapitalEstado = capitalDoEstado;
+        this.RegiaoDoEstado = regiaoEstado;
+        this.QuantMunicipios = quantMunicipios;
+        this.Next = proximo;
     }
-
 }
